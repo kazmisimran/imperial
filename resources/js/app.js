c46ec07687f1893d140ae00cb7695868a26477e3
Simran <kazmisimran@gmail.com>
2020-04-17 10:35:33 +0200

    test-encore

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+require('./../../node_modules/jquery/dist/jquery');
+require('./lib/jquery/jquery.min.js');
+global.$ = global.jQuery =require('jquery');
+
+require('./bootstrap.js');
+require('./../../node_modules/bootstrap/dist/js/bootstrap.bundle');
+require('./../../node_modules/bootstrap/dist/js/bootstrap');
+require('./../../node_modules/wow.js/dist/wow');
+require('./lib/superfish/hoverIntent');
+require('./lib/superfish/superfish');
+require('./lib/morphext/morphext');
+require('./lib/stickyjs/sticky');
+require('./lib/easing/easing');
+
+require('./js/custom.js');
+
+require('./contactform/contactform');
